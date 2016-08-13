6d5fd14bb26cb48fbffeee5484fe6665b426b784
Jason Davenport <jason.arkens17@gmail.com>
2016-08-12 19:23:03 -0700

    add controller

--- src/server/services/passport.js
+++ src/server/services/passport.js
@@ -1,5 +1,5 @@
 import passport from 'passport';
-import User from '../db/User';
+import User from '../models/User';
 import config from '../config';
 import LocalStrategy from 'passport-local';
 import JwtStrategy from 'passport-jwt.Strategy';
@@ -7,11 +7,11 @@ import ExtractJwt from 'passport-jwt.ExtractJwt';
 
 const localOptions = { usernnameField: 'email' };
 const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
-  User.findOne({ email: email }, function(err, user) {
+  User.findOrCreateUser({ email: email }, function(err, user) {
     if(err) { return done(err) };
     if(!user) { return done(null, false) };
 
-    user.comparePassword(password, function(err, isMatch) {
+    user.isValidPassword(password, function(err, isMatch) {
       if(err) { return done(err) };
       if(!isMatch) { return done(null, false) };
 
