89f338b31f5c58b9ddac39a14e1052f172ddc2c1
Alex Stark <alexcstark@gmail.com>
2016-08-12 17:16:45 -0700

    fix ssl error

--- src/db/dbm.js
+++ src/db/dbm.js
@@ -1,5 +1,7 @@
 // const DB_CONFIG_OBJ = require('../../secret/config').DB_CONFIG_OBJ;
 import Stork from './stork/index';
+import pg from 'pg';
+pg.defaults.ssl = true;
 
 export default new Stork({
   connection: process.env.DATABASE_URL,
