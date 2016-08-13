91052fd1a415b7f95685dee4a2c95fb9a04ef3a9
Alex Stark <alexcstark@gmail.com>
2016-08-12 20:22:23 -0700

    fix update routes

--- src/server/models/Ride.js
+++ src/server/models/Ride.js
@@ -1,25 +1,30 @@
 import {Ride} from '../../db/Ride';
 export const addRide = function(req, res) {
   Ride.create(req.body)
-  .then((ride) => res.json(ride));
+  .then((ride) => res.json(ride))
+  .catch((err) => res.json(err));
 };
 
 export const getRidesForUser = function (req, res) {
   Ride.find({userId: req.params.userid})
-  .then((rides) => res.json(rides));
+  .then((rides) => res.json(rides))
+  .catch((err) => res.json(err));
 };
 
 export const updateRide = function (req, res) {
-  Ride.update(req.body)
-  .then((ride) => res.json(ride));
+  Ride.update({id: req.params.rideid}, req.body)
+  .then((ride) => res.json(ride))
+  .catch((err) => res.json(err));
 };
 
 export const getAllRideData = function(req, res) {
-  Ride.getAll()
-  .then((rides) => res.json(rides));
+  Ride.findAll()
+  .then((rides) => res.json(rides))
+  .catch((err) => res.json(err));
 };
 
 export const deleteRide = function (req, res) {
-  Ride.delete(req.body)
-  .then((ride) => res.json(ride));
+  Ride.delete({id: req.params.rideid})
+  .then((ride) => res.json(ride))
+  .catch((err) => res.json(err));
 };
--- src/server/models/User.js
+++ src/server/models/User.js
@@ -2,26 +2,31 @@ import {User} from '../../db/User';
 
 export const getUserDashboardData = (req, res) => {
   User.find({id: req.params.userid})
-  .then((user) => res.json(user));
+  .then((user) => res.json(user))
+  .catch((err) => res.json(err));
 };
 
 export const updateUserData = (req, res) => {
-  User.update({id: req.params.userid})
-  .then((user) => res.json(user));
+  User.update({id: req.params.userid}, req.body)
+  .then((user) => res.json(user))
+  .catch((err) => res.json(err));
 };
 
 export const createUser = (req, res) => {
   User.create(req.body)
-  .then((user) => res.json(user));
+  .then((user) => res.json(user))
+  .catch((err) => res.json(err));
 };
 
 export const getAllUserData = (req, res) => {
   User.findAll()
-  .then((users) => res.json(users));
+  .then((users) => res.json(users))
+  .catch((err) => res.json(err));
 };
 
 
 export const findOrCreateUser = (req, res) => {
   User.findOrCreate(req.body)
-  .then((user) => res.json(user));
+  .then((user) => res.json(user))
+  .catch((err) => res.json(err));
 };
