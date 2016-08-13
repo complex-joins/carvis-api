5e2cdf53c37ea029b9db1ecb69139c76fa100ae4
Christopher Pruijsen <christopher.pruijsen@gmail.com>
2016-08-12 17:27:52 -0700

    updated DB schema for user and rides

--- src/db/Ride.js
+++ src/db/Ride.js
@@ -1,16 +1,33 @@
 import dbm from './dbm';
 
 export const RideSchema = function (ride) {
-  ride.increments('id').primary();
-  ride.integer('userId');
-  ride.string('selectedRide', 255);
-  ride.string('origin', 255);
-  ride.string('destination', 255);
-  ride.string('lyftEstimatedFare', 255);
-  ride.string('lyftEstimatedDuration', 100);
+  ride.increments('id')
+    .primary();
+  ride.timestamps();
+  ride.integer('userId'); // foreign key for the User table |-> carvis ID
+
+  // default: 'estimate' - on booked: 'booked' - on cancel: 'cancelled'
+  ride.string('rideStatus', 255);
+
+  // the origin and destination of the ride - set before estimations
+  ride.string('originLat', 255);
+  ride.string('originLng', 255);
+  ride.string('originRoutableAddress', 255);
+  ride.string('destinationLat', 255);
+  ride.string('destinationLng', 255);
+  ride.string('destinationRoutableAddress', 255);
+
+  // below is returned on the public estimate calls
+  ride.string('vendorRideType', 255); // ex: 'pool', 'line', etc.
+  ride.string('lyftEstimatedFare', 255); // dollars and cents
+  ride.string('lyftEstimatedDuration', 100); // minutes
   ride.string('uberEstimatedFare', 255);
   ride.string('uberEstimatedDuration', 100);
-  ride.timestamps();
+  ride.string('winner', 255); // the vendor we book with - ex. 'uber'
+
+  // below is returned on the request-ride calls
+  ride.string('eta', 255); // actual ETA of the specific booked car
+  ride.string('vendorRideId', 255); // vendor string to reference cancellations
 };
 
 export const Ride = dbm.model('rides');
--- src/db/User.js
+++ src/db/User.js
@@ -1,17 +1,55 @@
 import dbm from './dbm';
 export const UserSchema = function (user) {
-  user.increments('id').primary();
-  user.string('email', 100).unique();
-  user.string('password', 100);
-  user.string('lyftPhoneNumber', 100);
-  user.string('lyftToken', 100);
-  user.string('uberToken', 100);
-  user.string('uberEmailAddress', 100);
-  user.string('uberPassword', 100);
-  user.string('homeLatitude', 100);
-  user.string('homeLongitude', 100);
-  user.string('homeAddress', 100);
+  user.increments('id')
+    .primary();
   user.timestamps();
+
+  // carvis auth -- might be factored out.
+  user.string('email', 100)
+    .unique();
+  user.string('password', 100);
+
+  // the Amazon Alexa userId
+  user.string('alexaUserId', 100)
+    .unique();
+
+  // location shortcut data from our own web client
+  // we offer users the ability to set these to the Lyft shortcuts,
+  // in case we have those - or specify new shortcuts.
+  user.string('carvisHomeLat', 255);
+  user.string('carvisHomeLng', 255);
+  user.string('carvisHomeRoutableAddress', 255);
+  user.string('carvisWorkLat', 255);
+  user.string('carvisWorkLng', 255);
+  user.string('carvisWorkRoutableAddress', 255);
+
+  // data returned from lyft specific functions
+  user.string('firstName', 100);
+  user.string('lastName', 100);
+  user.string('picture', 100);
+  user.string('lyftEmail', 100);
+  // the phone number used for lyft authentication
+  user.string('lyftPhoneNumber', 100);
+  // data lyft uses for request-ride calls
+  user.string('lyftPaymentInfo', 100);
+  user.string('lyftToken', 100)
+    .unique();
+  // location shortcut data returned from Lyft
+  user.string('lyftHomeLat', 255);
+  user.string('lyftHomeLng', 255);
+  user.string('lyftHomeRoutableAddress', 255);
+  user.string('lyftWorkLat', 255);
+  user.string('lyftWorkLng', 255);
+  user.string('lyftWorkRoutableAddress', 255);
+
+  // data returned from uber specific functions
+  // token used for uber request-ride
+  user.string('uberToken', 100)
+    .unique();
+  // data used for uber authentication
+  user.string('uberEmail', 100);
+  user.string('uberPassword', 100);
+
 };
 
 export const User = dbm.model('users');
