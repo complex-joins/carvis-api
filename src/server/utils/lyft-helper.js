d017bde78f5a22012149e9b35ac12e5c99c8a554
Christopher Pruijsen <christopher.pruijsen@gmail.com>
2016-08-15 17:32:11 -0700

    uberhelper includes DB posts

--- src/server/utils/lyft-helper.js
+++ src/server/utils/lyft-helper.js
@@ -161,6 +161,7 @@ var requestRide = function (token, costToken, destination, origin, paymentInfo, 
     .then(function (data) {
       console.log('successful requestRide post LYFT', data);
       var response = lyftMethods.requestRide.responseMethod(data, userId, tripDuration);
+
       // var dbpostURL = 'http://' + APIserver + '/rides/' + rideId;
       var dbpostURL = 'http://localhost:8080/rides/' + rideId;
 
