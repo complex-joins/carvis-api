a3e7ea18d58e57e9987f217269886b06fc6438d3
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-08-16 12:36:46 -0700

    Fix merge conflict during rebase

---
+++ src/server/controllers/alexa.js
@@ -0,0 +1,10 @@
+exports.handleIntent = function(req, res) {
+  res.json({response: 'inside alexa.handleIntent'});
+  // const email = req.body.email;
+
+  // if (!email || !password) {
+  //   return res.status(422).send({ error: 'You must provide email and password'});
+  // }
+
+  // res.json({ token: tokenForUser(user) });
+};
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,9 +1,12 @@
+import {getRidesForUser, addRide, updateRide, getAllRideData, deleteRide} from '../models/Ride';
+import {getUserDashboardData, updateUserData, createUser,
+        getAllUserData, findOrCreateUser, updateOrCreateUser, deleteUser } from '../models/User';
 // import passport from 'passport';
 // import passportService from './services/passport';
-import RideController from './controllers/RideController';
-import UserController from './controllers/UserController';
 import { rawUserData } from './models/User';
+import Authentication from './controllers/authentication';
 import hasValidAPIToken from './server-configuration/hasValidAPIToken';
+import alexa from './controllers/alexa';
 
 export default function(app) {
   // TODO only let the user with that ID find users (middleware);
@@ -11,8 +14,32 @@ export default function(app) {
     res.status(200).send('Welcome to the Carvis API.');
   });
 
-  UserController(app);
-  RideController(app);
+  app.get('/users/:userid', getUserDashboardData);
+
+  app.get('/dev/users', hasValidAPIToken, getAllUserData);
+
+  app.post('/dev/users', hasValidAPIToken, createUser);
+
+  app.post('/auth/users', findOrCreateUser);
+
+  app.post('/users/updateOrCreate', updateOrCreateUser);
+
+  app.put('/users/update/:userid', updateUserData);
+
+  app.delete('/dev/users/:userid', deleteUser);
+
+  app.get('/dev/rides', getAllRideData);
+
+  app.get('/rides/user/:userid', getRidesForUser);
+
+  app.put('/rides/:rideid', updateRide);
+
+  app.post('/rides', addRide);
+
+  app.delete('/rides/:rideid', deleteRide);
+
+  app.post('/alexa', alexa.handleIntent);
+
   // app.post('/signin', requireSignin, Authentication.signin);
   // app.post('/signup', Authentication.signup);
 
