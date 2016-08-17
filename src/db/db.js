25a8d3b98ebed94c1b425e9143f5f761dd699cea
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-08-17 15:19:31 -0700

    Add more logging to debug Travis

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,7 @@
 import Stork from 'storkSQL';
 console.log('before requiring in db/db.js');
-const DB_CONFIG_OBJ = process.env.DB_CONFIG_JSON ? JSON.parse(DB_CONFIG_JSON) : require('../../secret/config').DB_CONFIG_OBJ;
+console.log('DB_CONFIG_JSON:', process.env.DB_CONFIG_JSON);
+const DB_CONFIG_OBJ = (process.env.DB_CONFIG_JSON) ? JSON.parse(DB_CONFIG_JSON) : require('../../secret/config').DB_CONFIG_OBJ;
 console.log('after requiring in db/db.js');
 
 
