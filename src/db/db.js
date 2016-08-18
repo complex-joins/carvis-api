9f111f30f8ed1b41b9fdda13ffe38edd142ff9f8
Alex Stark <alexcstark@gmail.com>
2016-08-17 17:02:05 -0700

    testing env

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,7 @@
 import Stork from 'storkSQL';
-const DB_CONFIG_OBJ = process.env.DB_CONFIG_JSON ? JSON.parse(DB_CONFIG_JSON) : require('../../secret/config').DB_CONFIG_OBJ;
-
-const dbConnection = new Stork(DB_CONFIG_OBJ, 'pg');
+// const DB_CONFIG_OBJ =
+console.log(process.env.newest);
+// console.log(DB_CONFIG_OBJ, typeof DB_CONFIG_OBJ);
+// let db = {host:"ec2-54-163-245-32.compute-1.amazonaws.com","password":"q5n2_cI-kAXn3ILwTwTOncJqjk","database":"d58chdd0ccaivt","port":5432,"user":"vabewtuoqwydkd","ssl":true};
+const dbConnection = new Stork(JSON.parse(process.env.DB_CONFIG_OBJ_JSON), 'pg');
 export default dbConnection;
