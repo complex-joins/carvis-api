1b80cc23678fc64c7167c00872431a8b8f79c8e7
Alex Stark <alexcstark@gmail.com>
2016-08-17 20:13:27 -0700

    db connection

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,12 @@
 import Stork from 'storkSQL';
-// const DB_CONFIG_OBJ =
-console.log(process.env.newest);
-// console.log(DB_CONFIG_OBJ, typeof DB_CONFIG_OBJ);
-// let db = {host:"ec2-54-163-245-32.compute-1.amazonaws.com","password":"q5n2_cI-kAXn3ILwTwTOncJqjk","database":"d58chdd0ccaivt","port":5432,"user":"vabewtuoqwydkd","ssl":true};
+import pg from 'pg';
+pg.defaults.ssl = true;
+
 const dbConnection = new Stork(JSON.parse(process.env.DB_CONFIG_OBJ_JSON), 'pg');
 export default dbConnection;
+
+
+//
+// function parseConnectionString() {
+//   process.env.DB_CONFIG
+// }
