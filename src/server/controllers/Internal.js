b95576f611452a29708529173019b468134d4c8a
Christopher Pruijsen <christopher.pruijsen@gmail.com>
2016-08-20 14:04:39 -0700

    change flow on error getLyftBearer

--- src/server/controllers/Internal.js
+++ src/server/controllers/Internal.js
@@ -4,8 +4,6 @@ const CARVIS_HELPER_API_KEY = process.env.CARVIS_HELPER_API_KEY;
 
 // NOTE: the below sets and gets the bearer tokens -- invoked on post and get to the API server route '/internal/lyftBearerToken', called from the helper API where the logic to refresh the token lives.
 
-// on client consumption of the token, on error you should call the helper api to handle the refresh.
-
 // on client consumption - instead of looking in process.env - check redis with `getLyftToken`
 
 export const updateLyftToken = (req, res) => {
@@ -13,28 +11,39 @@ export const updateLyftToken = (req, res) => {
   redisSetKeyWithExpire('lyftBearerToken', 84600, token /*, cb*/ );
 
   // refresh call to the helper API on expire time -- this will on success do a post to this API / function again (making it recursive at interval)
-  const refreshToken = () => {
-    let helperURL = CARVIS_HELPER_API + '/lyft/refreshBearerToken';
-    fetch(helperURL, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-access-token': CARVIS_HELPER_API_KEY
-        }
-      })
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        console.log('success refreshToken', data);
-      })
-      .catch(err => {
-        console.warn('error refreshing token', err);
-      });
-  };
   setTimeout(refreshToken, 84600);
 };
 
 export const getLyftToken = (req, res) => {
-  redisGetKey('lyftBearerToken', /*, cb*/ ); // returns value when no callback
+
+  // returns value when no callback
+  let token = redisGetKey('lyftBearerToken', /*, cb*/ );
+
+  if (!token) {
+    refreshToken();
+    // TODO: return to client after updating and setting on Redis
+    // interval check to see if token in redis, and if found cancel interval?
+  } else {
+    return token; // change to res.send() ?
+  }
+};
+
+const refreshToken = () => {
+  let helperURL = CARVIS_HELPER_API + '/lyft/refreshBearerToken';
+  fetch(helperURL, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-access-token': CARVIS_HELPER_API_KEY
+      }
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log('success refreshToken', data);
+    })
+    .catch(err => {
+      console.warn('error refreshing token', err);
+    });
 };
