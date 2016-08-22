b209530efb5a408c8f0b9380dfd0aad19ec1b030
Alex Stark <alexcstark@gmail.com>
2016-08-22 16:27:44 -0700

    env variables for docker

--- src/db/db.js
+++ src/db/db.js
@@ -1,3 +1,4 @@
 import Stork from 'storkSQL';
+console.log('env variables', process.env);
 const dbConnection = new Stork(JSON.parse(process.env.DB_CONFIG_OBJ_JSON), 'pg');
 export default dbConnection;
