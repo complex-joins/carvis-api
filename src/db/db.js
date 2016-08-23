2d3068a73890a2acc2d29594d83ad38d52e77569
Alex Stark <alexcstark@gmail.com>
2016-08-22 19:05:48 -0700

    db json problem

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,10 @@
 import Stork from 'storkSQL';
 const DB_CONFIG = convertDB_CONFIG(process.env.DB_CONFIG_OBJ_JSON);
-const dbConnection = new Stork(JSON.parse(process.env.DB_CONFIG_OBJ_JSON), 'pg');
+const dbConnection = new Stork(JSON.parse(process.env.DB_CONFIG_OBJ_JSON.slice(0, -1)), 'pg');
 export default dbConnection;
 
 
 function convertDB_CONFIG(input){
-  let test = "{'password':'q5n2_cI-kAXn3ILwTwTOncJqjk','database':'d58chdd0ccaivt','port':5432,'host':'ec2-54-163-245-32.compute-1.amazonaws.com','user':'vabewtuoqwydkd','ssl':true}";
   let DB_CONFIG;
   if (input.indexOf("'password'") >= 0) {
     DB_CONFIG = input.replace(/\'/g, '"');
@@ -14,3 +13,6 @@ function convertDB_CONFIG(input){
   }
   return DB_CONFIG;
 }
+
+
+console.log("test".slice(0, -1));
