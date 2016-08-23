684490c96847e73166a618327f964aad8f5f9e78
Alex Stark <alexcstark@gmail.com>
2016-08-22 19:18:17 -0700

    break db config into object

--- src/db/db.js
+++ src/db/db.js
@@ -1,18 +1,16 @@
 import Stork from 'storkSQL';
-const DB_CONFIG = convertDB_CONFIG(process.env.DB_CONFIG_OBJ_JSON);
-const dbConnection = new Stork(JSON.parse(process.env.DB_CONFIG_OBJ_JSON.slice(0, -1)), 'pg');
-export default dbConnection;
-
-
-function convertDB_CONFIG(input){
-  let DB_CONFIG;
-  if (input.indexOf("'password'") >= 0) {
-    DB_CONFIG = input.replace(/\'/g, '"');
-  } else {
-    DB_CONFIG = input;
-  }
-  return DB_CONFIG;
+let DB_CONFIG;
+if (process.env.AWS) {
+  DB_CONFIG = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+    ssl: true
+  };
+} else {
+  DB_CONFIG = JSON.parse(process.env.DB_CONFIG_OBJ_JSON);
 }
 
-
-console.log("test".slice(0, -1));
+const dbConnection = new Stork(DB_CONFIG, 'pg');
+export default dbConnection;
