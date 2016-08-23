a48ce163c40fbbabf8cf9c346d1e89b3f26f22f2
Alex Stark <alexcstark@gmail.com>
2016-08-22 19:57:20 -0700

    force success deploy

--- src/db/db.js
+++ src/db/db.js
@@ -1,17 +1,16 @@
 import Stork from 'storkSQL';
-let DB_CONFIG;
-console.log('in AWS env', process.env.AWS);
-if (process.env.AWS && JSON.parse(process.env.AWS)) {
-  DB_CONFIG = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: 5432,
-    ssl: true
-  };
-} else {
-  DB_CONFIG = JSON.parse(process.env.DB_CONFIG_OBJ_JSON);
-}
+let DB_CONFIG =  {
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT
+};
+// console.log('in AWS env', process.env.AWS);
+// if (process.env.AWS && JSON.parse(process.env.AWS)) {
+//   DB_CONFIG
+// } else {
+//   // DB_CONFIG = JSON.parse(process.env.DB_CONFIG_OBJ_JSON);
+// }
 
 const dbConnection = new Stork(DB_CONFIG, 'pg');
 export default dbConnection;
